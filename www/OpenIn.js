50bc0b7b97a344ac987850c9fe583c08e8360e00
lhdmulder <denis.mulder@santecure.com>
2017-09-18 09:36:38 -0400

    Add files via upload

---
+++ www/OpenIn.js
@@ -0,0 +1,57 @@
+var exec = require('cordova/exec');
+
+exports.coolMethod = function(arg0, success, error) {
+    exec(success, error, "OpenIn", "coolMethod", [arg0]);
+};
+var exec = require('cordova/exec'),
+  channel = require('cordova/channel'),
+
+  // Reference name for the plugin
+  PLUGIN_NAME = 'OpenIn',
+
+
+// Plugin methods on the native side that can be called from JavaScript
+pluginNativeMethod = {
+  SUBSCRIBE: 'jsSubscribeForEvent',
+  UNSUBSCRIBE: 'jsUnsubscribeFromEvent'
+};
+
+var openin = {
+
+  /**
+   * Subscribe to event.
+   * If plugin already captured that event - callback will be called immidietly.
+   *
+
+   * @param {Function} callback - callback that is called when event is captured
+   */
+  subscribe: function(callback) {
+    if (!callback) {
+      console.warn('OpenIn: can\'t subscribe to event without a callback');
+      return;
+    }
+
+ 
+
+    var innerCallback = function(msg) {
+      callback(msg.data);
+    };
+
+    exec(innerCallback, null, PLUGIN_NAME, pluginNativeMethod.SUBSCRIBE);
+  },
+
+  /**
+   * Unsubscribe from the event.
+   *
+   * @param {String} eventName - from what event we are unsubscribing
+   */
+  unsubscribe: function(eventName) {
+    if (!eventName) {
+      eventName = DEFAULT_EVENT_NAME;
+    }
+
+    exec(null, null, PLUGIN_NAME, pluginNativeMethod.UNSUBSCRIBE);
+  }
+};
+
+module.exports = openin;
