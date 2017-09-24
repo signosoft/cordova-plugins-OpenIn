e4fb387924330c43b194bff4bfacde7d6993891e
lhdmulder <denis.mulder@santecure.com>
2017-09-24 11:14:09 -0400

    Add files via upload

--- src/ios/OpenIn.m
+++ src/ios/OpenIn.m
@@ -4,32 +4,24 @@
 
 @implementation OpenIn
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
-    self.launchedURL = [launchOptions objectForKey:UIApplicationLaunchOptionsURLKey];
-    return YES;
 
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-    CDVPluginResult* pluginResult = nil;
-    NSString *URLstring=[self.launchedURL absoluteString];
-    
-    if (self.launchedURL) {
-         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString: URLstring];
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:self.callbackId];
-        self.launchedURL = nil;
-    }
-}
 
 #pragma mark Methods, available from JavaScript side
 
 - (void)jsSubscribeForEvent:(CDVInvokedUrlCommand *)command {
-
-   
+    
+    
     self.callbackId = command.callbackId;
     self.launchedURL = nil;
+}
+
+- (BOOL)handleUrl:(NSURL *)url {
+    CDVPluginResult* pluginResult = nil;
+    NSString *URLstring=[url absoluteString];
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString: URLstring];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:self.callbackId];
+    url = nil;
+    return YES;
 }
 
 @end
