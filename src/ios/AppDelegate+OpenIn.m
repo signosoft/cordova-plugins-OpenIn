e844dbf3bca6c3e3cd17e39d109afde7826d310b
lhdmulder <denis.mulder@santecure.com>
2017-09-25 16:27:40 -0400

    New load method

--- src/ios/AppDelegate+OpenIn.m
+++ src/ios/AppDelegate+OpenIn.m
@@ -35,13 +35,37 @@ static NSString *const PLUGIN_NAME = @"OpenIn";
     return returnvalue;
 }
 
-+ (void)load
-{
-    Method original, swizzled;
++ (void)load {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+         Class class = object_getClass((id)self);
+
+        SEL originalSelector = @selector(application:openURL:options:);
+        SEL swizzledSelector = @selector(sw_application:openURL:options:);
+
+
+
+
+      
     
-    original = class_getInstanceMethod(self, @selector(application:openURL:options:));
-    swizzled = class_getInstanceMethod(self, @selector(sw_application:openURL:options:));
-    method_exchangeImplementations(original, swizzled);
+     Method originalMethod = class_getClassMethod(class, originalSelector);
+ Method swizzledMethod = class_getClassMethod(class, swizzledSelector);
+
+        BOOL didAddMethod =
+            class_addMethod(class,
+                originalSelector,
+                method_getImplementation(swizzledMethod),
+                method_getTypeEncoding(swizzledMethod));
+
+        if (didAddMethod) {
+            class_replaceMethod(class,
+                swizzledSelector,
+                method_getImplementation(originalMethod),
+                method_getTypeEncoding(originalMethod));
+        } else {
+            method_exchangeImplementations(originalMethod, swizzledMethod);
+        }
+    });
 }
 
 @end
