cdf758e44b59235d87c76980cbcc05955988cadb
lhdmulder <denis.mulder@santecure.com>
2017-09-25 09:44:36 -0400

    Update AppDelegate+OpenIn.m
    Swizzle implemented

--- src/ios/AppDelegate+OpenIn.m
+++ src/ios/AppDelegate+OpenIn.m
@@ -8,27 +8,40 @@
 
 #import "AppDelegate+OpenIn.h"
 #import "OpenIn.h"
+#import <objc/runtime.h>
 
 @implementation AppDelegate (OpenIn)
 
 static NSString *const PLUGIN_NAME = @"OpenIn";
 
-- (BOOL)application:(UIApplication *)app
+- (BOOL)sw_application:(UIApplication *)app
             openURL:(NSURL *)url
             options:(NSDictionary<UIApplicationOpenURLOptionsKey, id> *)options;
 {
- 
+    BOOL returnvalue;
+    returnvalue= [self sw_application:(UIApplication *)app
+                              openURL:(NSURL *)url
+                              options:(NSDictionary<UIApplicationOpenURLOptionsKey, id> *)options];
     if (url == nil) {
-        return NO;
+        return returnvalue;
     }
     
     // get instance of the plugin and let it handle the url object
     OpenIn *plugin = [self.viewController getCommandInstance:PLUGIN_NAME];
     if (plugin == nil) {
-        return NO;
+        return returnvalue;
     }
+    [plugin handleUrl:url];
+    return returnvalue;
+}
+
++ (void)load
+{
+    Method original, swizzled;
     
-    return [plugin handleUrl:url];
+    original = class_getInstanceMethod(self, @selector(application:openURL:options:));
+    swizzled = class_getInstanceMethod(self, @selector(sw_application:openURL:options:));
+    method_exchangeImplementations(original, swizzled);
 }
 
 @end
