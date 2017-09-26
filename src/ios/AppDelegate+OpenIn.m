f5c356e0311533903c43f81aa28956d51c784040
lhdmulder <denis.mulder@santecure.com>
2017-09-26 12:10:26 -0400

    Update AppDelegate+OpenIn.m

--- src/ios/AppDelegate+OpenIn.m
+++ src/ios/AppDelegate+OpenIn.m
@@ -14,7 +14,7 @@
 
 static NSString *const PLUGIN_NAME = @"OpenIn";
 
-- (BOOL)sw_application:(UIApplication *)app
+- (BOOL)application:(UIApplication *)app
                openURL:(NSURL *)url
                options:(NSDictionary<UIApplicationOpenURLOptionsKey, id> *)options;
 {
@@ -23,90 +23,22 @@ static NSString *const PLUGIN_NAME = @"OpenIn";
                               openURL:url
                               options:options];
     if (url == nil) {
-        [self sw_application:app
-                     openURL:url
-                     options:options];
+
         return NO;
     }
     
     // get instance of the plugin and let it handle the url object
     OpenIn *plugin = [self.viewController getCommandInstance:PLUGIN_NAME];
     if (plugin == nil) {
-        [self sw_application:app
-                     openURL:url
-                     options:options];
+
         return NO;
     }
     NSLog( @"1st url is %@", [url absoluteString]);
     [plugin handleUrl:url];
     NSLog(@"The code runs through here!");
     NSLog( @"2nd url is %@", [url absoluteString]);
-    [self sw_application:app
-                 openURL:url
-                 options:options];
+
     return YES;
-}
-
-// void defaultMethodIMP (id self, SEL _cmd) { /* nothing to do here */ }
-
-void DumpObjcMethods(Class clz) {
-    
-    unsigned int methodCount = 0;
-    Method *methods = class_copyMethodList(clz, &methodCount);
-    
-    NSLog(@"Found %d methods on '%s'\n", methodCount, class_getName(clz));
-    
-    for (unsigned int i = 0; i < methodCount; i++) {
-        Method method = methods[i];
-        
-        NSLog(@"\t'%s' has method named '%s' of encoding '%s'\n",
-              class_getName(clz),
-              sel_getName(method_getName(method)),
-              method_getTypeEncoding(method));
-        
-        /**
-         *  Or do whatever you need here...
-         */
-    }
-    
-    free(methods);
-}
-
-
-+ (void)load {
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        Class class = [self class];
-
-        SEL originalSelector = @selector(application:openURL:options:);
-        SEL swizzledSelector = @selector(sw_application:openURL:options:);
-
-        Method originalMethod = class_getInstanceMethod(class, originalSelector);
-        Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
-
-        // When swizzling a class method, use the following:
-        // Class class = object_getClass((id)self);
-        // ...
-        // Method originalMethod = class_getClassMethod(class, originalSelector);
-        // Method swizzledMethod = class_getClassMethod(class, swizzledSelector);
-
-        BOOL didAddMethod =
-            class_addMethod(class,
-                originalSelector,
-                method_getImplementation(swizzledMethod),
-                method_getTypeEncoding(swizzledMethod));
-
-        if (didAddMethod) {
-            class_replaceMethod(class,
-                swizzledSelector,
-                method_getImplementation(originalMethod),
-                method_getTypeEncoding(originalMethod));
-        } else {
-            method_exchangeImplementations(originalMethod, swizzledMethod);
-        }
-    });
-       DumpObjcMethods(self);
-    DumpObjcMethods(object_getClass(self) /* Metaclass */);
 }
 
 
