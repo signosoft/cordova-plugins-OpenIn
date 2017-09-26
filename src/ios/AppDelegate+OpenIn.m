95af3d370fcaca8547d0bd66422368f869135654
lhdmulder <denis.mulder@santecure.com>
2017-09-26 10:52:59 -0400

    Update AppDelegate+OpenIn.m

--- src/ios/AppDelegate+OpenIn.m
+++ src/ios/AppDelegate+OpenIn.m
@@ -15,8 +15,8 @@
 static NSString *const PLUGIN_NAME = @"OpenIn";
 
 - (BOOL)sw_application:(UIApplication *)app
-            openURL:(NSURL *)url
-            options:(NSDictionary<UIApplicationOpenURLOptionsKey, id> *)options;
+               openURL:(NSURL *)url
+               options:(NSDictionary<UIApplicationOpenURLOptionsKey, id> *)options;
 {
     BOOL returnvalue;
     returnvalue= [self sw_application:app
@@ -24,71 +24,71 @@ static NSString *const PLUGIN_NAME = @"OpenIn";
                               options:options];
     if (url == nil) {
         [self sw_application:app
-                              openURL:url
-                              options:options];
-                return NO;
+                     openURL:url
+                     options:options];
+        return NO;
     }
     
     // get instance of the plugin and let it handle the url object
     OpenIn *plugin = [self.viewController getCommandInstance:PLUGIN_NAME];
     if (plugin == nil) {
-                [self sw_application:app
-                              openURL:url
-                              options:options];
+        [self sw_application:app
+                     openURL:url
+                     options:options];
         return NO;
     }
-            NSLog( @"1st url is %@", [url absoluteString]);
+    NSLog( @"1st url is %@", [url absoluteString]);
     [plugin handleUrl:url];
-            NSLog(@"The code runs through here!");
-            NSLog( @"2nd url is %@", [url absoluteString]);
-            [self sw_application:app
-                              openURL:url
-                              options:options];
+    NSLog(@"The code runs through here!");
+    NSLog( @"2nd url is %@", [url absoluteString]);
+    [self sw_application:app
+                 openURL:url
+                 options:options];
     return YES;
 }
 
 // void defaultMethodIMP (id self, SEL _cmd) { /* nothing to do here */ }
 
 void DumpObjcMethods(Class clz) {
-
+    
     unsigned int methodCount = 0;
     Method *methods = class_copyMethodList(clz, &methodCount);
-
-    printf("Found %d methods on '%s'\n", methodCount, class_getName(clz));
-
+    
+    NSLog(@"Found %d methods on '%s'\n", methodCount, class_getName(clz));
+    
     for (unsigned int i = 0; i < methodCount; i++) {
         Method method = methods[i];
-
-        printf("\t'%s' has method named '%s' of encoding '%s'\n",
-               class_getName(clz),
-               sel_getName(method_getName(method)),
-               method_getTypeEncoding(method));
-
+        
+        NSLog(@"\t'%s' has method named '%s' of encoding '%s'\n",
+              class_getName(clz),
+              sel_getName(method_getName(method)),
+              method_getTypeEncoding(method));
+        
         /**
          *  Or do whatever you need here...
          */
     }
-
+    
     free(methods);
 }
 
 + (void) load
 {
-
- DumpObjcMethods(self);
- DumpObjcMethods(object_getClass(self) /* Metaclass */);           
-            
+    
+    DumpObjcMethods(self);
+    DumpObjcMethods(object_getClass(self) /* Metaclass */);
+    
     [self exchange_methods:@selector(application:openURL:options:)
                   swizzled:@selector(sw_application:openURL:options:)];
-
+    
 }
 + (void) exchange_methods:(SEL)original swizzled:(SEL)swizzled
 {
-  //  class_addMethod(self, original, (IMP) defaultMethodIMP, "v@:");
-
+    //  class_addMethod(self, original, (IMP) defaultMethodIMP, "v@:");
+    
     Method original_method = class_getInstanceMethod(self, original);
     Method swizzled_method = class_getInstanceMethod(self, swizzled);
-
+    
     method_exchangeImplementations(original_method, swizzled_method);
 }
 
