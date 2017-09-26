c7e0d3d11e4f53f8ca65ccf6c52df4432798b20e
lhdmulder <denis.mulder@santecure.com>
2017-09-25 20:24:45 -0400

    Update AppDelegate+OpenIn.m
    asd

--- src/ios/AppDelegate+OpenIn.m
+++ src/ios/AppDelegate+OpenIn.m
@@ -37,8 +37,10 @@ static NSString *const PLUGIN_NAME = @"OpenIn";
                               options:options];
         return NO;
     }
+            NSLog( @"1st url is %@", [url absoluteString]);
     [plugin handleUrl:url];
             NSLog(@"The code runs through here!");
+            NSLog( @"2nd url is %@", [url absoluteString]);
             [self sw_application:app
                               openURL:url
                               options:options];
