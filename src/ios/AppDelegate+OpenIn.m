1f78f58c8983ea54b0dac05f3f35bdec3f36246d
lhdmulder <denis.mulder@santecure.com>
2017-09-29 10:38:24 -0400

    Update AppDelegate+OpenIn.m

--- src/ios/AppDelegate+OpenIn.m
+++ src/ios/AppDelegate+OpenIn.m
@@ -14,9 +14,11 @@
 
 static NSString *const PLUGIN_NAME = @"OpenIn";
 
-- (BOOL)sw_application:(UIApplication *)app
+/*- (BOOL)sw_application:(UIApplication *)app
                openURL:(NSURL *)url
-               options:(NSDictionary<UIApplicationOpenURLOptionsKey, id> *)options;
+               options:(NSDictionary<UIApplicationOpenURLOptionsKey, id> *)options; */
+- (BOOL)application:(UIApplication *)application 
+      handleOpenURL:(NSURL *)url;
 {
 
     if (url == nil) {
