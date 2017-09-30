69161fff26e7458178a9e3ce14db207c5255fe93
lhdmulder <denis.mulder@santecure.com>
2017-09-30 18:07:07 -0400

    Update OpenIn.m

--- src/ios/OpenIn.m
+++ src/ios/OpenIn.m
@@ -18,6 +18,11 @@
 - (BOOL)handleUrl:(NSURL *)url {
     CDVPluginResult* pluginResult = nil;
     NSLog(@"The code runs through handleurl!");
+    
+           DumpObjcMethods(self);
+    DumpObjcMethods(object_getClass(self) /* Metaclass */);
+    
+    
    if (! [[url scheme] isEqual:@"file"]) {
         NSLog(@"The code runs through not a file!");
         return NO;
@@ -32,5 +37,28 @@
 
    
 }
+void DumpObjcMethods(Class clz) {
+    
+    unsigned int methodCount = 0;
+    Method *methods = class_copyMethodList(clz, &methodCount);
+    
+    NSLog(@"Found %d methods on '%s'\n", methodCount, class_getName(clz));
+    
+    for (unsigned int i = 0; i < methodCount; i++) {
+        Method method = methods[i];
+        
+        NSLog(@"\t'%s' has method named '%s' of encoding '%s'\n",
+              class_getName(clz),
+              sel_getName(method_getName(method)),
+              method_getTypeEncoding(method));
+        
+        /**
+         *  Or do whatever you need here...
+         */
+    }
+    
+    free(methods);
+}
+
 
 @end
