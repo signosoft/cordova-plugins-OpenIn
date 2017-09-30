1f68f13febbdbb94d02a6612a7bfc9004e0fd116
lhdmulder <denis.mulder@santecure.com>
2017-09-30 18:05:06 -0400

    Update AppDelegate+OpenIn.m

--- src/ios/AppDelegate+OpenIn.m
+++ src/ios/AppDelegate+OpenIn.m
@@ -34,9 +34,7 @@ static NSString *const PLUGIN_NAME = @"OpenIn";
     NSLog( @"1st url is %@", [url absoluteString]);
     [plugin handleUrl:url];
     NSLog( @"2nd url is %@", [url absoluteString]);
-  NSMutableDictionary* dictionary = @{}.mutableCopy;
-  [self application:app openURL:url
-  sourceApplication:@&amp;quot;&amp;quot; annotation:dictionary]
+
 
     return YES;
 }
