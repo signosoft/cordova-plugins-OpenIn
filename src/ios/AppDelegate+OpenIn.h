554f108f51657fd0ce71d1eb26f19e0763b90904
lhdmulder <denis.mulder@santecure.com>
2017-09-30 17:29:28 -0400

    Update AppDelegate+OpenIn.h

--- src/ios/AppDelegate+OpenIn.h
+++ src/ios/AppDelegate+OpenIn.h
@@ -7,17 +7,9 @@
 //
 
 #import "AppDelegate.h"
-#import <UIKit/UIKit.h>
-#import <CoreData/CoreData.h>
-#import "Infrastructure.h"
-#import "DeepLinkController.h"
-#import "OSNavigationController.h"
 
 
 @interface AppDelegate (OpenIn)
-
-@property (strong, nonatomic) DeepLinkController *deepLinkController;
-@property (strong, nonatomic) NSMutableArray *trustedHosts;
 
 - (BOOL)application:(UIApplication *)app
                openURL:(NSURL *)url
