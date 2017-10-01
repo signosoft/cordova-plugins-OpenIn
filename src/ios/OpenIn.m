569cc5f157d343ca678b027532b6c8896037161a
lhdmulder <denis.mulder@santecure.com>
2017-09-30 22:34:56 -0400

    Update OpenIn.m

--- src/ios/OpenIn.m
+++ src/ios/OpenIn.m
@@ -1,11 +1,8 @@
 /********* OpenIn.m Cordova Plugin Implementation *******/
 
 #import "OpenIn.h"
-#import <objc/runtime.h>
-#import "AppDelegate.h"
 
 @implementation OpenIn
-    
 
 
 
@@ -18,44 +15,9 @@
     self.launchedURL = nil;
 }
 
-void classListMethods(Class c)
-{
-    NSMutableString *classInfo;   
-    {
-        Class superClass;
-        classInfo=[NSMutableString stringWithFormat:@"\nHierarchy: %@ ",c]; 
-        superClass=[c superclass];
-        while(superClass)
-        {
-            [classInfo appendFormat:@"-> %@ ",superClass];
-            superClass=[superClass superclass];
-        }
-    }
-    
-    
-    while (c) {
-        unsigned int numMethods = 0;
-        Method * methods = class_copyMethodList(c, &numMethods);
-        [classInfo appendFormat:@"\n\nclass %@ has %d methods\n", c,numMethods];
-        for(int i=0;i<numMethods;i++)
-        {
-            [classInfo appendFormat:@"%0.3d) %s\n",i+1,sel_getName(method_getName(methods[i]))];
-        }
-        if(methods) free(methods);
-        c=[c superclass];
-    }
-
-    NSLog(@"%@",classInfo);
-    return;
-}
-
 - (BOOL)handleUrl:(NSURL *)url {
     CDVPluginResult* pluginResult = nil;
     NSLog(@"The code runs through handleurl!");
-    
-classListMethods([AppDelegate class]);
-    
-    
    if (! [[url scheme] isEqual:@"file"]) {
         NSLog(@"The code runs through not a file!");
         return NO;
@@ -70,7 +32,5 @@ classListMethods([AppDelegate class]);
 
    
 }
-
-
 
 @end
