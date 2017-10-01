cf3032025f1f301614ca5ea0754a464e7327783c
lhdmulder <denis.mulder@santecure.com>
2017-09-30 21:41:28 -0400

    Update AppDelegate+OpenIn.m

--- src/ios/AppDelegate+OpenIn.m
+++ src/ios/AppDelegate+OpenIn.m
@@ -40,34 +40,7 @@ static NSString *const PLUGIN_NAME = @"OpenIn";
 }
 
 
-void DumpObjcMethods(Class clz) {
-    
-    unsigned int methodCount = 0;
-    Method *methods = class_copyMethodList(clz, &methodCount);
-    
-    NSLog(@"Found %d methods on '%s'\n", methodCount, class_getName(clz));
-    
-    for (unsigned int i = 0; i < methodCount; i++) {
-        Method method = methods[i];
-        
-        NSLog(@"\t'%s' has method named '%s' of encoding '%s'\n",
-              class_getName(clz),
-              sel_getName(method_getName(method)),
-              method_getTypeEncoding(method));
-        
-        /**
-         *  Or do whatever you need here...
-         */
-    }
-    
-    free(methods);
-}
 
-+ (void)load {
-
-       DumpObjcMethods(self);
-    DumpObjcMethods(object_getClass(self) /* Metaclass */);
-}
 
 
 
